3a7441c8d515d05e14bbd79f30d041922f356be0
Nomost <guillaume.soufflet1@viacesi.fr>
2019-06-24 09:37:40 +0200

    project structure

---
+++ src/Services/Device/Device.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Device.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ src/Services/MetricPersistence/src/main/java/exakis/atlantis/metricpersistence/MetricPersistenceApplication.java
@@ -0,0 +1,13 @@
+package exakis.atlantis.MetricPersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricPersistenceApplication.class, args);
+	}
+
+}
