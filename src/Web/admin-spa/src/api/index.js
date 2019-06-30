ce3b4fef21f36e4571ff4b0a50a21da9fbba1ab8
Florian Dias <florian.dias@viacesi.fr>
2019-07-01 00:36:17 +0200

    Allow to associate a user to a device

--- src/Web/admin-spa/src/api/index.js
+++ src/Web/admin-spa/src/api/index.js
@@ -11,13 +11,15 @@ const webservice = axios.create({
 webservice.interceptors.response.use(response => {
     console.log('Ajax response: ', response);
     return response.data;
-}, response => {
-    if (response.status === 401) {
-        if (localStorage.getItem('access_token') & localStorage.getItem('token_type')) {
+}, error => {
+    console.log('response: ', error)
+    console.log('response: ', error.response)
+    if (error.response.status === 401) {
+        if (localStorage.getItem('access_token') && localStorage.getItem('token_type')) {
             localStorage.removeItem('access_token');
             localStorage.removeItem('token_type');
-            window.location.href = "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_type=token&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&scope=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790%20openid&state=D9aUT4AWYPbEmL2htHtfVmNTNUH3umtisNaNYP-ctCo%3D&redirect_uri=http://localhost:8090/login"
         }
+        window.location.href = "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_type=token&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&scope=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790%20openid&state=D9aUT4AWYPbEmL2htHtfVmNTNUH3umtisNaNYP-ctCo%3D&redirect_uri=http://localhost:8090/login"
     }
 })
 
@@ -27,4 +29,4 @@ export const getUsers = () => webservice.get('/users');
 
 export const addDeviceToUser = (userId, device) => webservice.post('/users/' + userId + '/devices', device);
 
-export const addUserToDevice = (deviceId, user) => webservice.post('/devices' + deviceId + '/users', user);
+export const addUserToDevice = (deviceId, user) => webservice.post('/devices/' + deviceId + '/user', user);
