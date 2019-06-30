3d255d006759f45ebb238a287a82a4fb07493462
Nomost <guillaume.soufflet1@viacesi.fr>
2019-07-01 01:43:45 +0200

    Calculation types endpoint in Mobile API

---
+++ src/Services/Mobile.API/src/main/java/exakis/atlantis/mobileapi/controllers/CalculationController.java
@@ -0,0 +1,24 @@
+package exakis.atlantis.mobileapi.controllers;
+
+import exakis.atlantis.mobileapi.dtos.CalculationType;
+import exakis.atlantis.mobileapi.dtos.MqttResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("calculations")
+public class CalculationController {
+    @GetMapping("types")
+    @ResponseBody
+    public Iterable<CalculationType> getCalculationTypes() {
+        RestTemplate restTemplate = new RestTemplate();
+        CalculationType[] types = restTemplate.getForObject("http://localhost:5000/api/calculations/calculation_types", CalculationType[].class);
+        return Arrays.asList(types);
+    }
+}
